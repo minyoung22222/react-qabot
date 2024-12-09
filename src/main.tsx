4d60706d843673abe63f0af1541ddcebbae7b8c6
minyoung <alsduddl9977@gmail.com>
2024-12-09 14:32:37 +0900

    rename: styles to src

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "../styles/gloabal.css";
+import "./styles/gloabal.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
