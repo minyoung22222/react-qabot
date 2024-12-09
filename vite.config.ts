076b209d5ff150c4d222adb09b328740d1a855a6
minyoung <alsduddl9977@gmail.com>
2024-12-09 14:58:34 +0900

    hotfix: not include index.html in dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "react-qabot",
+      fileName: (format) => `qabot.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    emptyOutDir: true,
+  },
   plugins: [react()],
-})
+});
