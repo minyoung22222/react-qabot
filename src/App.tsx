7bc7a548f129d017dd0c0b6bef03fe8933f8f36a
minyoung <alsduddl9977@gmail.com>
2024-12-31 14:28:12 +0900

    feat: add & use CustomCursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import CustomCursor from "./components/CustomCursor";
 import Qabot from "./components/Qabot";
 import QaButton from "./components/QaButton";
 import Toast from "./components/Toast";
+import { useQaModeStore } from "./stores/qaModeStore";
 import "./styles/gloabal.css";
 import { QabotProps } from "./types";
 
 function App(props: QabotProps) {
+  const { qaMode } = useQaModeStore();
+
   return (
     <div>
       <Toast />
+      {qaMode && <CustomCursor />}
       <Qabot env={props.env ?? ""} />
       <QaButton />
     </div>
